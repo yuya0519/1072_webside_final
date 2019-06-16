d7c38843cd697579d038e114c04c8b0045713497
Tanyayayayaayyayayaya <50259239+Tanyayayayaayyayayaya@users.noreply.github.com>
2019-06-16 13:39:02 +0800

    Add files via upload

--- RandomSelector/main.js
+++ RandomSelector/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     $("input").click(function()
     {
-        var numberofListItem=$("li").length;
+        var numberofListItem=$("li2").length;
         var randomChildNumber=Math.floor(Math.random()*numberofListItem);
         
-        $("H1").text($("li").eq(randomChildNumber).text());
+        $("H2").text($("li2").eq(randomChildNumber).text());
         document.getElementById("pic").setAttribute('src',randomChildNumber+".gif");
     });
 });
